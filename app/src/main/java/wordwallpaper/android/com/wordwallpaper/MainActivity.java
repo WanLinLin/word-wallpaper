b458f41b6ed161d4d084bfde2e9cc3016aa67bec
rickchung <rickjung88@gmail.com>
2016-06-19 16:08:49 +0800

    Add typeface def

--- app/src/main/java/wordwallpaper/android/com/wordwallpaper/MainActivity.java
+++ app/src/main/java/wordwallpaper/android/com/wordwallpaper/MainActivity.java
@@ -1,17 +1,22 @@
 package wordwallpaper.android.com.wordwallpaper;
 
+import android.graphics.Typeface;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Typeface myTypeface;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTypeface = Typeface.createFromAsset(getAssets(), "NotoSansCJKtc-Regular.otf");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setRippleColor(getResources().getColor(R.color.fabRipple));
