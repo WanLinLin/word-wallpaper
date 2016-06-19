6e408af5f5822d7e9089cecaf4ce3dbc10b6441a
rickchung <rickjung88@gmail.com>
2016-06-19 17:07:29 +0800

    Add snackbar of successful message

--- app/src/main/java/wordwallpaper/android/com/wordwallpaper/MainActivity.java
+++ app/src/main/java/wordwallpaper/android/com/wordwallpaper/MainActivity.java
@@ -5,24 +5,40 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = "MainActivity";
+
     Typeface myTypeface;
     Bitmap currentBitmap;
+    Snackbar snackbar;
+    CoordinatorLayout coordinatorLayout;
+    WallpaperManager wm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Define some useful variables
+        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.my_coordinatorlayout);
+        wm = WallpaperManager.getInstance(this);
+
         // Reserve Typeface
         myTypeface = Typeface.createFromAsset(getAssets(), "NotoSansCJKtc-Regular.otf");
+
+        // Init snackbar
+        snackbar = Snackbar.make(coordinatorLayout, getString(R.string.set_wallpaper_ok),
+                                 Snackbar.LENGTH_SHORT);
 
         // TODO: Generate bitmap event
         try {
@@ -35,15 +51,18 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //((EditText) findViewById(R.id.edit_text)).setText("Why don't you eat shit?");
-                if (currentBitmap != null) setAsWallpaper(currentBitmap);
+                if (currentBitmap != null) {
+                    setAsWallpaper(currentBitmap);
+                    currentBitmap = null;
+                    snackbar.show();
+                }
             }
         });
     }
 
     private void setAsWallpaper(Bitmap b) {
-        WallpaperManager wm = WallpaperManager.getInstance(getApplicationContext());
         try {
+            Log.d(LOG_TAG, "Try to set Bitmap");
             wm.setBitmap(b);
         } catch (IOException e) {
             e.printStackTrace();
