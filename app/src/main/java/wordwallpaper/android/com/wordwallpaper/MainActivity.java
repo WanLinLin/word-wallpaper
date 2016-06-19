6ddba6c3815dd820f31d5bbc81c636b4786e306a
wanlinlin <leo12345639@gmail.com>
2016-06-19 16:00:19 +0800

    ui

--- app/src/main/java/wordwallpaper/android/com/wordwallpaper/MainActivity.java
+++ app/src/main/java/wordwallpaper/android/com/wordwallpaper/MainActivity.java
@@ -1,7 +1,10 @@
 package wordwallpaper.android.com.wordwallpaper;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setRippleColor(getResources().getColor(R.color.fabRipple));
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((EditText) findViewById(R.id.edit_text)).setText("Why don't you eat shit?");
+            }
+        });
+
+
     }
 }
